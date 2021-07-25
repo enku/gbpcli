216c89086c606e22a991dde47116bb4f0452ae57
Albert Hopkins <marduk@letterboxes.org>
2021-07-25 08:12:58 -0700

    diff: allow defaults for left and right arguments
    If left is not speficied then should default to the published build.
    If  right is not specified then it should default to the latest build.

--- src/gbpcli/subcommands/diff.py
+++ src/gbpcli/subcommands/diff.py
@@ -1,27 +1,48 @@
-"""Show differences between two builds"""
+"""Show differences between two builds
+
+If the "left" argument is omitted, it defaults to the build which is published.
+
+If the "right" argument is omitted, it defaults to the most recent build.
+"""
 import argparse
+import sys
 
 from gbpcli import GBP, Status, utils
 
 
 def handler(args: argparse.Namespace, gbp: GBP) -> int:
     """Handler for subcommand"""
-    left_build, right_build, diff = gbp.diff(args.machine, args.left, args.right)
+    left = args.left
+    right = args.right
+
+    if left is None:
+        builds = gbp.builds(args.machine)
+        published = [i for i in builds if (i.info and i.info.published)]
+
+        if not published:
+            print("No origin specified and no builds published", file=sys.stderr)
+            return 1
+
+        assert len(published) == 1
+        left = published[0].number
+
+        assert right is None
+        right = builds[-1].number
+
+    if right is None:
+        right = gbp.latest(args.machine).number
+
+    left_build, right_build, diff = gbp.diff(args.machine, left, right)
 
     if not diff:
         return 0
 
-    print(f"diff -r {args.machine}/{args.left} {args.machine}/{args.right}")
     assert left_build.info is not None
-    print(
-        f"--- a/{args.machine}/{args.left}"
-        f" {utils.timestr(left_build.info.submitted)}"
-    )
     assert right_build.info is not None
-    print(
-        f"+++ b/{args.machine}/{args.right}"
-        f" {utils.timestr(right_build.info.submitted)}"
-    )
+
+    print(f"diff -r {args.machine}/{left} {args.machine}/{right}")
+    print(f"--- a/{args.machine}/{left} {utils.timestr(left_build.info.submitted)}")
+    print(f"+++ b/{args.machine}/{right} {utils.timestr(right_build.info.submitted)}")
 
     last_modified = None
     # for change, item in iter(response["diff"]["items"]):
@@ -43,5 +64,5 @@ def handler(args: argparse.Namespace, gbp: GBP) -> int:
 def parse_args(parser: argparse.ArgumentParser):
     """Set subcommand arguments"""
     parser.add_argument("machine", help="name of the machine")
-    parser.add_argument("left", type=int, help="left build number")
-    parser.add_argument("right", type=int, help="right build number")
+    parser.add_argument("left", type=int, nargs="?", help="left build number")
+    parser.add_argument("right", type=int, nargs="?", help="right build number")
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -45,3 +45,63 @@ diff -r lighthouse/2083 lighthouse/2084
         diff(args, gbp)
 
         self.assertEqual(print_mock.stdout.getvalue(), "")
+
+    def test_when_right_is_none_should_use_latest(self, _print_mock):
+        args = Namespace(machine="lighthouse", left=2083, right=None)
+        latest_json = parse(load_data("latest.json"))
+        mock_diff_json = parse(load_data("diff.json"))
+        gbp = make_gbp()
+        gbp.session.get.side_effect = (
+            make_response(json=latest_json),
+            make_response(json=mock_diff_json),
+        )
+
+        status = diff(args, gbp)
+
+        self.assertEqual(status, 0)
+        expected_calls = [
+            mock.call("http://test.invalid/api/builds/lighthouse/latest"),
+            mock.call("http://test.invalid/api/builds/lighthouse/diff/2083/2085"),
+        ]
+        gbp.session.get.assert_has_calls(expected_calls)
+
+    def test_when_left_is_none_should_use_published(self, _print_mock):
+        args = Namespace(machine="lighthouse", left=None, right=None)
+        list_json = parse(load_data("list.json"))
+        mock_diff_json = parse(load_data("diff.json"))
+        gbp = make_gbp()
+        gbp.session.get.side_effect = (
+            make_response(json=list_json),
+            make_response(json=mock_diff_json),
+        )
+
+        status = diff(args, gbp)
+
+        self.assertEqual(status, 0)
+        expected_calls = [
+            mock.call("http://test.invalid/api/builds/lighthouse/"),
+            mock.call("http://test.invalid/api/builds/lighthouse/diff/2080/2086"),
+        ]
+        gbp.session.get.assert_has_calls(expected_calls)
+
+    def test_when_left_is_none_and_not_published(self, print_mock):
+        args = Namespace(machine="lighthouse", left=None, right=None)
+        list_json = parse(load_data("list.json"))
+
+        # Make sure there are not published builds
+        for item in list_json["builds"]:
+            item["storage"]["published"] = False
+
+        gbp = make_gbp()
+        gbp.session.get.return_value = make_response(json=list_json)
+
+        status = diff(args, gbp)
+
+        self.assertEqual(status, 1)
+        gbp.session.get.assert_called_once_with(
+            "http://test.invalid/api/builds/lighthouse/"
+        )
+        self.assertEqual(
+            print_mock.stderr.getvalue(),
+            "No origin specified and no builds published\n",
+        )
