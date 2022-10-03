cb6b55933b1278788b39af10089be7a234d94d11
Albert Hopkins <marduk@letterboxes.org>
2022-10-03 09:12:41 -0500

    list: display built time instead of submitted time
    Old builds dont' have a built time, so fall back to submitted in that
    case.

--- src/gbpcli/subcommands/list.py
+++ src/gbpcli/subcommands/list.py
@@ -27,12 +27,17 @@ def handler(args: argparse.Namespace, gbp: GBP, console: Console) -> int:
     )
     table.add_column("Status")
     table.add_column("ID", justify="right")
-    table.add_column("Submitted")
+    table.add_column("Built")
     table.add_column("Tags")
 
     for build in builds:
         assert build.info is not None
-        timestamp = build.info.submitted.astimezone(LOCAL_TIMEZONE)
+
+        # In the old days, we didn't have a "built" field. Fall back to submitted
+        timestamp = (build.info.built or build.info.submitted).astimezone(
+            LOCAL_TIMEZONE
+        )
+
         status = (
             f"{'[magenta]*[/magenta]' if build.packages_built else ' '}"
             f"{'[yellow]K[/yellow]' if build.info.keep else ' '}"
@@ -40,10 +45,10 @@ def handler(args: argparse.Namespace, gbp: GBP, console: Console) -> int:
             f"{'[blue]N[/blue]' if build.info.note else ' '}"
         )
         number = f"[bold]{build.number}[/bold]"
-        submitted = f"{timestamp.strftime('%x %X')}"
+        built = f"{timestamp.strftime('%x %X')}"
         tag_list = [f"@{tag}" for tag in build.info.tags] if build.info.tags else []
         tags = f"[yellow]{' '.join(tag_list)}[/yellow]"
-        table.add_row(status, number, submitted, tags)
+        table.add_row(status, number, built, tags)
 
     console.print(table)
 
