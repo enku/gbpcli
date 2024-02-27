5eba3e5b3fc4bdc14fc4de1e46ab51d578c45ce0
Albert Hopkins <marduk@letterboxes.org>
2024-02-27 17:20:49 -0600

    add initial support for using config files
    In order to support API keys, we desire keys to be stored in a config
    file. This is the first step along that process.

---
+++ src/gbpcli/config.py
@@ -0,0 +1,51 @@
+"""gbpcli configuration
+
+This module adds helpers for reading an gbpcli configuration from a file. This can be
+used instead of or in addition to the command-line arguments.
+"""
+
+import typing as t
+from dataclasses import dataclass
+
+try:
+    import tomllib
+except ImportError:  # pragma: no cover
+    import toml as tomlib  # pylint: disable=unused-import
+
+
+SECTION = "gbpcli"
+
+
+_T = t.TypeVar("_T", bound="Config")
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class Config:
+    """gbpcli config file structure"""
+
+    url: str | None = None
+    my_machines: list[str] | None = None
+    api_key: str | None = None
+
+    @classmethod
+    def from_file(cls: type[_T], fp: t.IO[bytes]) -> _T:
+        """Return a Config instance given the config file"""
+        toml_data = tomllib.load(fp)
+        gbpcli_data = get_section(toml_data, SECTION)
+
+        return cls(**gbpcli_data)
+
+
+def get_section(toml_data: dict[str, t.Any], section: str) -> dict[str, t.Any]:
+    """Return the given section from the toml_data dict
+
+    If the given section does not exist, raise ConfigError
+    """
+    try:
+        return toml_data[section]
+    except KeyError as error:
+        raise ConfigError(f"Missing section: {section}") from error
+
+
+class ConfigError(Exception):
+    """Config errors"""
---
+++ tests/test_config.py
@@ -0,0 +1,51 @@
+# pylint: disable=missing-docstring
+import os.path
+import tempfile
+
+from gbpcli import config
+
+from . import TestCase
+
+
+class ConfigTests(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        tempdir = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+        self.addCleanup(tempdir.cleanup)
+        self.tempdir = tempdir.name
+        self.filename = os.path.join(self.tempdir, "gbpcli.toml")
+
+    def test_from_file(self) -> None:
+        with open(self.filename, "wb+") as fp:
+            fp.write(
+                b"""\
+[gbpcli]
+url = "http://test.invalid/"
+my_machines = ["babette", "lighthouse"]
+"""
+            )
+            fp.seek(0)
+            conf = config.Config.from_file(fp)
+
+        self.assertEqual(conf.url, "http://test.invalid/")
+        self.assertEqual(conf.my_machines, ["babette", "lighthouse"])
+        self.assertEqual(conf.api_key, None)
+
+    def test_missing_section(self):
+        with open(self.filename, "wb+") as fp:
+            fp.seek(0)
+
+            with self.assertRaises(config.ConfigError):
+                config.Config.from_file(fp)
+
+    def test_empty_section(self):
+        with open(self.filename, "wb+") as fp:
+            fp.write(b"[gbpcli]\n")
+            fp.seek(0)
+
+            conf = config.Config.from_file(fp)
+
+        self.assertEqual(conf.url, None)
+        self.assertEqual(conf.my_machines, None)
+        self.assertEqual(conf.api_key, None)
