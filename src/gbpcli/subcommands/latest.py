283d7ae237c9762b23033f419a17c37015fba075
Albert Hopkins <marduk@letterboxes.org>
2024-09-13 23:02:05 -0500

    latest: handler: simplify the logic

--- src/gbpcli/subcommands/latest.py
+++ src/gbpcli/subcommands/latest.py
@@ -11,13 +11,12 @@ HELP = """Show the latest build number for the given machine"""
 
 def handler(args: argparse.Namespace, gbp: GBP, console: Console) -> int:
     """Show the latest build number for a machine"""
-    if not (latest_build := gbp.latest(args.machine)):
-        console.err.print("No builds exist for the given machine")
-        return 1
+    if latest_build := gbp.latest(args.machine):
+        console.out.print(latest_build.number)
+        return 0
 
-    console.out.print(latest_build.number)
-
-    return 0
+    console.err.print("No builds exist for the given machine")
+    return 1
 
 
 def parse_args(parser: argparse.ArgumentParser) -> None:
