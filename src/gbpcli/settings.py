1f57b2e7b76417f21ec352d7abfcd56237be05d2
Albert Hopkins <marduk@letterboxes.org>
2025-01-26 11:16:36 -0600

    add BaseSettings from gentoo-build-publisher
    There are plugins that (want to) use BaseSettings, however it is defined
    in gentoo-build-publisher, so they must pull in all of gbp. Since a lot
    of these have cli components we don't want to have to pull all of gbp in
    just to import Settings (which actually might not even be used by the
    cli).  Therefore must move it into gbpcli and have Gentoo Build
    Publisher use that (gentoo-build-publisher already depends on gbpcli).

---
+++ src/gbpcli/settings.py
@@ -0,0 +1,67 @@
+"""From gbpcli"""
+
+import os
+from dataclasses import dataclass, fields
+from pathlib import Path
+from typing import Any, ClassVar, Self
+
+from strtobool import strtobool  # type: ignore
+
+
+@dataclass(frozen=True)
+class BaseSettings:
+    """Base class for Settings
+
+    Subclasses should define me as the prefix for environment variables for these
+    settings. For example if prefix is "BUILD_PUBLISHER_" and the field is named "FOO"
+    then the environment variable for that field is "BUILD_PUBLISHER_FOO"
+    """
+
+    env_prefix: ClassVar = ""
+
+    @classmethod
+    def from_dict(cls: type[Self], prefix: str, data_dict: dict[str, Any]) -> Self:
+        """Return Settings instantiated from a dict"""
+        params: dict[str, Any] = {}
+
+        for field in fields(cls):
+            if (key := f"{prefix}{field.name}") not in data_dict:
+                continue
+
+            assert isinstance(field.type, type)
+            params[field.name] = string_value_to_field_value(data_dict[key], field.type)
+
+        return cls(**params)
+
+    @classmethod
+    def from_environ(cls: type[Self], prefix: str | None = None) -> Self:
+        """Return settings instantiated from environment variables"""
+        if prefix is None:
+            prefix = cls.env_prefix
+
+        return cls.from_dict(prefix, dict(os.environ))
+
+
+def string_value_to_field_value(value: str, type_: type[Any]) -> Any:
+    """Coerse the given string value to the given type"""
+    if type_ is bool:
+        return get_bool(value)
+
+    if type_ is int:
+        return int(value)
+
+    if type_ is Path:
+        return Path(value)
+
+    return value
+
+
+def get_bool(value: str | bytes | bool) -> bool:
+    """Return the boolean value of the truthy/falsey string"""
+    if isinstance(value, bool):
+        return value
+
+    if isinstance(value, bytes):
+        value = value.decode("UTF-8")
+
+    return bool(strtobool(value))
---
+++ tests/test_settings.py
@@ -0,0 +1,62 @@
+"""Tests for GBP Settings"""
+
+# pylint: disable=missing-class-docstring,missing-function-docstring
+from dataclasses import dataclass
+from pathlib import Path
+from typing import ClassVar
+from unittest import TestCase, mock
+
+from gbpcli.settings import BaseSettings
+
+
+@dataclass(frozen=True)
+class Settings(BaseSettings):
+    env_prefix: ClassVar = "BUILD_PUBLISHER_"
+
+    # pylint: disable=invalid-name
+    STORAGE_PATH: Path
+    FLAG: bool
+    JENKINS_DOWNLOAD_CHUNK_SIZE: int
+    JENKINS_USER: str | None = None
+
+
+class SettingsTestCase(TestCase):
+    def test_from_environ(self) -> None:
+        data_dict = {
+            "BUILD_PUBLISHER_JENKINS_USER": "fail",
+            "TODAY_IS": "your birthday",
+            "TODAY_JENKINS_BASE_URL": "https://jenkins.invalid/",
+            "TODAY_JENKINS_DOWNLOAD_CHUNK_SIZE": "14",
+            "TODAY_FLAG": "no",
+            "TODAY_STORAGE_PATH": "/home/today",
+        }
+
+        with mock.patch.dict("gbpcli.settings.os.environ", data_dict, clear=True):
+            settings = Settings.from_environ(prefix="TODAY_")
+
+        self.assertEqual(settings.STORAGE_PATH, Path("/home/today"))
+        self.assertEqual(settings.JENKINS_USER, None)
+        self.assertEqual(settings.JENKINS_DOWNLOAD_CHUNK_SIZE, 14)
+        self.assertIs(settings.FLAG, False)
+
+    def test_from_dict(self) -> None:
+        data_dict = {
+            "BUILD_PUBLISHER_JENKINS_USER": "fail",
+            "TODAY_IS": "your birthday",
+            "TODAY_JENKINS_BASE_URL": "https://jenkins.invalid/",
+            "TODAY_JENKINS_DOWNLOAD_CHUNK_SIZE": "14",
+            "TODAY_STORAGE_PATH": "/home/today",
+            "TODAY_FLAG": "yes",
+        }
+        prefix = "TODAY_"
+
+        settings = Settings.from_dict(prefix, data_dict)
+
+        self.assertEqual(settings.STORAGE_PATH, Path("/home/today"))
+        self.assertEqual(settings.JENKINS_USER, None)
+        self.assertEqual(settings.JENKINS_DOWNLOAD_CHUNK_SIZE, 14)
+        self.assertIs(settings.FLAG, True)
+
+        with self.assertRaises(AttributeError):
+            # pylint: disable=no-member,pointless-statement
+            settings.IS  # type: ignore
