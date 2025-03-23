eb1774969feab298be7fcef1870009bdb291d11f
Albert Hopkins <marduk@letterboxes.org>
2025-03-23 11:37:59 -0500

    noxfile: run lint as well as the tests

--- noxfile.py
+++ noxfile.py
@@ -12,5 +12,6 @@ def tests(session: nox.Session) -> None:
     ]
     session.install(*dev_dependencies)
 
-    session.run("coverage", "run", "-m", "tests")
+    session.run("pdm", "run", "make", "test", external=True)
+    session.run("pdm", "run", "make", "lint", external=True)
     session.run("coverage", "report")
