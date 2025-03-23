aa22057cdca328e4090011cb4d1c92438e250190
Albert Hopkins <marduk@letterboxes.org>
2025-03-23 11:30:16 -0500

    tests: add a noxfile
    Mamma said nox you out!

---
+++ noxfile.py
@@ -0,0 +1,16 @@
+"""noxfile for ci/cd testing"""
+
+# pylint: disable=missing-docstring
+import nox
+
+
+@nox.session(python=("3.11", "3.12", "3.13", "3.14"))
+def tests(session: nox.Session) -> None:
+    session.install(".")
+    dev_dependencies = nox.project.load_toml("pyproject.toml")["dependency-groups"][
+        "dev"
+    ]
+    session.install(*dev_dependencies)
+
+    session.run("coverage", "run", "-m", "tests")
+    session.run("coverage", "report")
