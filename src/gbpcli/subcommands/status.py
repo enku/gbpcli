b1e28a6273623565a542bb9f2f5fb067facf2774
Albert Hopkins <marduk@letterboxes.org>
2025-03-30 12:23:46 -0500

    status: extract grid building into seperate functions

--- src/gbpcli/subcommands/status.py
+++ src/gbpcli/subcommands/status.py
@@ -10,7 +10,7 @@ from rich.table import Table
 from gbpcli import GBP
 from gbpcli.render import styled_yes, timestr, yesno
 from gbpcli.subcommands import completers as comp
-from gbpcli.types import Console, Package
+from gbpcli.types import Build, Console, Package
 from gbpcli.utils import resolve_build_id
 
 HELP = """Show details for a given build"""
@@ -26,39 +26,60 @@ def handler(args: argparse.Namespace, gbp: GBP, console: Console) -> int:
 
     assert build.info is not None
 
+    grid = create_grid()
+    col2 = f"[machine]{build.machine} [build_id]{build.number}[/build_id][/machine]"
+    add_row(grid, "Build", col2)
+    add_timestamps_to_grid(grid, build)
+    add_row(grid, "Published", f"{styled_yes(yesno(build.info.published))}")
+    add_row(grid, "Keep", f"{styled_yes(yesno(build.info.keep))}")
+    tags = [f"[tag]@{tag}[/tag]" for tag in build.info.tags]
+    add_row(grid, "Tags", " ".join(tags))
+    add_packages(build.packages_built, grid)
+    console.out.print(Panel(grid, expand=False, style="box"))
+    print_note(console, build.info.note)
+
+    return 0
+
+
+def create_grid() -> Table:
+    """Create and return a grid with the specified number of columns"""
     grid = Table.grid()
     grid.add_column()
     grid.add_column()
 
-    grid.add_row(
-        "[header]Build:[/header] ",
-        f"[machine]{build.machine} [build_id]{build.number}[/build_id][/machine]",
-    )
+    return grid
 
+
+def add_timestamps_to_grid(grid: Table, build: Build) -> None:
+    """Add build timestamps to the grid as rows.
+
+    Timestamps include:
+
+        - build time
+        - submitted time
+        - completed time
+    """
+    assert build.info is not None
     if build.info.built is not None:
         timestamp_row("BuildDate", build.info.built, grid)
     timestamp_row("Submitted", build.info.submitted, grid)
     timestamp_row("Completed", build.info.completed, grid)
 
-    grid.add_row(
-        "[header]Published:[/header] ", f"{styled_yes(yesno(build.info.published))}"
-    )
-    grid.add_row("[header]Keep:[/header] ", f"{styled_yes(yesno(build.info.keep))}")
-    tags = [f"[tag]@{tag}[/tag]" for tag in build.info.tags]
-    grid.add_row("[header]Tags:[/header] ", " ".join(tags))
-    add_packages(build.packages_built, grid)
 
-    console.out.print(Panel(grid, expand=False, style="box"))
+def print_note(console: Console, note: str | None) -> None:
+    """Print the given note to console.out
 
-    if note := build.info.note:
-        console.out.print()
-        table = Table(box=box.ROUNDED, pad_edge=False, style="box")
-        table.add_column("📎 Notes", header_style="header")
-        table.add_row("[note]" + note.rstrip("\n") + "[/note]")
+    If not is None, do nothing.
+    """
+    if not note:
+        return
 
-        console.out.print(table)
+    console.out.print()
+    table = Table(box=box.ROUNDED, pad_edge=False, style="box")
+    table.add_column("📎 Notes", header_style="header")
+    table.add_row("[note]" + note.rstrip("\n") + "[/note]")
 
-    return 0
+    console.out.print(table)
 
 
 # pylint: disable=duplicate-code
@@ -74,24 +95,28 @@ def parse_args(parser: argparse.ArgumentParser) -> None:
     )
 
 
-def timestamp_row(header: str, timestamp: dt.datetime | None, table: Table) -> None:
+def timestamp_row(header: str, timestamp: dt.datetime | None, grid: Table) -> None:
     """Add a header with a timestamp"""
-    table.add_row(
-        f"[header]{header}:[/header] ",
-        (
-            f"[timestamp]{timestr(timestamp)}[/timestamp]"
-            if timestamp
-            else styled_yes(yesno(False))
-        ),
-    )
+    if timestamp:
+        col2 = f"[timestamp]{timestr(timestamp)}[/timestamp]"
+    else:
+        col2 = styled_yes(yesno(False))
+    add_row(grid, header, col2)
 
 
-def add_packages(packages: list[Package] | None, table: Table) -> None:
+def add_packages(packages: list[Package] | None, grid: Table) -> None:
     """Add the packages header and list"""
     packages = packages or []
-    table.add_row(
-        "[header]Packages-built:[/header] ",
+    add_row(
+        grid,
+        "Packages-built",
         f"[package]{packages[0].cpv}[/package]" if packages else "None",
     )
     for package in packages[1:]:
-        table.add_row("", f"[package]{package.cpv}[/package]")
+        add_row(grid, "", f"[package]{package.cpv}[/package]")
+
+
+def add_row(grid: Table, col1: str, col2: str) -> None:
+    """Add  2-column row to the given grid"""
+    sep = ":" if col1 else ""
+    grid.add_row(f"[header]{col1}{sep} [/header]", col2)
