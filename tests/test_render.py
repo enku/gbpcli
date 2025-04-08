0a1de186819bca4da19e96b393d4494b38291ae9
Albert Hopkins <marduk@letterboxes.org>
2025-04-08 07:13:24 -0500

    tests: add test for render.build_to_str

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,9 +1,12 @@
 # pylint: disable=missing-docstring
 import argparse
 import datetime as dt
-from unittest import TestCase
+from unittest import TestCase, mock
 
-from gbpcli.render import format_machine, format_tags, timestr, yesno
+from gbpcli.render import build_to_str, format_machine, format_tags, timestr, yesno
+from gbpcli.types import Build, BuildInfo, Package
+
+from . import LOCAL_TIMEZONE
 
 
 class YesNoTestCase(TestCase):
@@ -24,6 +27,59 @@ class TimestrTestCase(TestCase):
         timezone = dt.timezone(dt.timedelta(days=-1, seconds=61200), "PDT")
 
         self.assertEqual(timestr(timestamp, timezone), "Tue Jul 20 09:45:06 2021 -0700")
+
+
+@mock.patch("gbpcli.render.LOCAL_TIMEZONE", new=LOCAL_TIMEZONE)
+class BuildToStrTests(TestCase):
+    build = Build(
+        machine="babette",
+        number=12,
+        info=BuildInfo(
+            keep=False,
+            note="This is a test",
+            published=True,
+            tags=["@foo", "@bar"],
+            submitted=dt.datetime(2025, 4, 8, 7, 1, tzinfo=dt.UTC),
+            built=dt.datetime(2025, 4, 8, 6, 1, tzinfo=dt.UTC),
+            completed=dt.datetime(2025, 4, 8, 7, 10, tzinfo=dt.UTC),
+        ),
+        packages_built=[
+            Package(
+                cpv="app-arch/libarchive-3.7.9",
+                build_time=dt.datetime(2025, 4, 8, 6, 10, tzinfo=dt.UTC),
+            ),
+            Package(
+                cpv="sys-apps/acl-2.3.2-r2",
+                build_time=dt.datetime(2025, 4, 8, 6, 20, tzinfo=dt.UTC),
+            ),
+        ],
+    )
+
+    def test(self) -> None:
+        result = build_to_str(self.build)
+
+        expected = """\
+[bold]Build:[/bold] [blue]babette/12[/blue]
+[bold]BuildDate:[/bold] Mon Apr  7 23:01:00 2025 -0700
+[bold]Submitted:[/bold] Tue Apr  8 00:01:00 2025 -0700
+[bold]Completed:[/bold] Tue Apr  8 00:10:00 2025 -0700
+[bold]Published:[/bold] yes
+[bold]Keep:[/bold] no
+[bold]Tags:[/bold] @foo @bar
+[bold]Packages-built:[/bold]
+    app-arch/libarchive-3.7.9
+    sys-apps/acl-2.3.2-r2
+
+This is a test
+
+"""
+        self.assertEqual(expected, result)
+
+    def test_no_info(self) -> None:
+        build = Build(machine="babette", number=12)
+
+        with self.assertRaises(ValueError):
+            build_to_str(build)
 
 
 class FormatTagsTest(TestCase):
