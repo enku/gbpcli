3addad7df433dc4fc4ec7dad0965ced99db26965
Albert Hopkins <marduk@letterboxes.org>
2025-08-20 07:44:35 -0500

    tests: use the testkit.environ fixture
    Remove the custom mock and use gbp-testkit's environ fixture.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,10 +1,14 @@
 """Tests for GBP Settings"""
 
-# pylint: disable=missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-docstring,unused-argument
+import os
 from dataclasses import dataclass
 from pathlib import Path
 from typing import ClassVar
-from unittest import TestCase, mock
+from unittest import TestCase
+
+import gbp_testkit.fixtures as testkit
+from unittest_fixtures import Fixtures, given, where
 
 from gbpcli.settings import BaseSettings, string_value_to_field_value
 
@@ -22,8 +26,10 @@ class Settings(BaseSettings):
     JENKINS_USER: str | None = None
 
 
+@given(testkit.environ)
+@where(environ__clear=True)
 class SettingsTestCase(TestCase):
-    def test_from_environ(self) -> None:
+    def test_from_environ(self, fixtures: Fixtures) -> None:
         data_dict = {
             "BUILD_PUBLISHER_JENKINS_USER": "fail",
             "TODAY_IS": "your birthday",
@@ -32,16 +38,16 @@ class SettingsTestCase(TestCase):
             "TODAY_FLAG": "no",
             "TODAY_STORAGE_PATH": "/home/today",
         }
+        os.environ.update(data_dict)
 
-        with mock.patch.dict("gbpcli.settings.os.environ", data_dict, clear=True):
-            settings = Settings.from_environ(prefix="TODAY_")
+        settings = Settings.from_environ(prefix="TODAY_")
 
         self.assertEqual(settings.STORAGE_PATH, Path("/home/today"))
         self.assertEqual(settings.JENKINS_USER, None)
         self.assertEqual(settings.JENKINS_DOWNLOAD_CHUNK_SIZE, 14)
         self.assertIs(settings.FLAG, False)
 
-    def test_from_dict(self) -> None:
+    def test_from_dict(self, fixtures: Fixtures) -> None:
         data_dict = {
             "BUILD_PUBLISHER_JENKINS_USER": "fail",
             "TODAY_IS": "your birthday",
