0e7bfe6dde12d360c69b936878f7220fcbbb5dfd
Albert Hopkins <marduk@letterboxes.org>
2025-08-21 11:19:36 -0500

    tests: publish: use testkit.gbp instead of lib.gbp
    This is the first phase of getting rid of tests.lib.gbp and
    tests.lib.make_response.  Since gbp-testkit's gbp fixture can make real
    calls to Gentoo Build Publisher, we shouldn't neet to mock GraphQL
    responses. This makes tests more reliable.

--- tests/test_publish.py
+++ tests/test_publish.py
@@ -3,6 +3,8 @@
 # pylint: disable=missing-function-docstring,protected-access
 import gbp_testkit.fixtures as testkit
 from gbp_testkit.helpers import parse_args
+from gentoo_build_publisher import publisher
+from gentoo_build_publisher.types import Build
 from unittest_fixtures import Fixtures, given
 
 from gbpcli.subcommands.publish import handler as publish
@@ -10,33 +12,36 @@ from gbpcli.subcommands.publish import handler as publish
 from . import lib
 
 
-@given(lib.gbp, testkit.console, lib.local_timezone)
+@given(testkit.gbp, testkit.console, lib.local_timezone, testkit.publisher)
 class PublishTestCase(lib.TestCase):
     """publish() tests"""
 
     def test(self, fixtures: Fixtures):
+        build = Build(machine="lighthouse", build_id="3109")
+        publisher.pull(build)
         cmdline = "gbp publish lighthouse 3109"
         args = parse_args(cmdline)
         gbp = fixtures.gbp
         console = fixtures.console
-        lib.make_response(gbp, "publish.json")
 
+        self.assertFalse(publisher.published(build))
         publish(args, gbp, console)
 
-        self.assert_graphql(gbp, gbp.query.gbpcli.publish, id="lighthouse.3109")
+        self.assertTrue(publisher.published(build))
 
     def test_should_get_latest_when_number_is_none(self, fixtures: Fixtures):
+        builds = [
+            Build(machine="lighthouse", build_id="3109"),
+            Build(machine="lighthouse", build_id="3110"),
+        ]
+        publisher.pull(builds[0])
+        publisher.pull(builds[1])  # <- latest
         cmdline = "gbp publish lighthouse"
         args = parse_args(cmdline)
         gbp = fixtures.gbp
         console = fixtures.console
-        lib.make_response(gbp, {"data": {"latest": {"id": "lighthouse.2080"}}})
-        lib.make_response(gbp, "publish.json")
 
         status = publish(args, gbp, console)
 
         self.assertEqual(status, 0)
-        self.assert_graphql(gbp, gbp.query.gbpcli.latest, index=0, machine="lighthouse")
-        self.assert_graphql(
-            gbp, gbp.query.gbpcli.publish, index=1, id="lighthouse.2080"
-        )
+        self.assertTrue(publisher.published(builds[1]))
