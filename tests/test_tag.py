72487dc586d82cfecfe991d668cf927f1a173b34
Albert Hopkins <marduk@letterboxes.org>
2025-08-23 14:42:28 -0500

    tests: tag: use testkit.gbpcli instead of lib.gbp

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -2,63 +2,46 @@
 
 # pylint: disable=missing-docstring,protected-access
 import gbp_testkit.fixtures as testkit
-from gbp_testkit.helpers import parse_args
-from unittest_fixtures import Fixtures, given
-
-from gbpcli.subcommands.tag import handler as tag
+from gentoo_build_publisher import publisher
+from gentoo_build_publisher.types import Build
+from unittest_fixtures import Fixtures, given, where
 
 from . import lib
 
+BUILD = Build(machine="lighthouse", build_id="9400")
 
-@given(lib.gbp, testkit.console)
+
+@given(testkit.gbpcli, lib.pulled_build)
+@where(pulled_build__build=BUILD)
 class TagTestCase(lib.TestCase):
     """tag() tests"""
 
     def test_tag(self, fixtures: Fixtures):
-        cmdline = "gbp tag lighthouse 9400 prod"
-        args = parse_args(cmdline)
-        gbp = fixtures.gbp
-        lib.make_response(gbp, "tag_build.json")
-
-        status = tag(args, gbp, fixtures.console)
+        status = fixtures.gbpcli("gbp tag lighthouse 9400 prod")
 
         self.assertEqual(status, 0)
-        self.assert_graphql(
-            gbp, gbp.query.gbpcli.tag_build, id="lighthouse.9400", tag="prod"
-        )
+        self.assertEqual(publisher.tags(BUILD), ["prod"])
 
     def test_untag(self, fixtures: Fixtures):
-        cmdline = "gbp tag -r lighthouse prod"
-        args = parse_args(cmdline)
-        gbp = fixtures.gbp
-        lib.make_response(gbp, "untag_build.json")
+        publisher.tag(BUILD, "prod")
 
-        status = tag(args, gbp, fixtures.console)
+        status = fixtures.gbpcli("gbp tag -r lighthouse prod")
 
         self.assertEqual(status, 0)
-        self.assert_graphql(
-            gbp, gbp.query.gbpcli.untag_build, machine="lighthouse", tag="prod"
-        )
+        self.assertEqual(publisher.tags(BUILD), [])
 
     def test_untag_with_string_starting_with_tagsym_works(self, fixtures: Fixtures):
-        cmdline = "gbp tag -r lighthouse @prod"
-        args = parse_args(cmdline)
-        gbp = fixtures.gbp
-        lib.make_response(gbp, "untag_build.json")
+        publisher.tag(BUILD, "prod")
 
-        status = tag(args, gbp, fixtures.console)
+        status = fixtures.gbpcli("gbp tag -r lighthouse @prod")
 
         self.assertEqual(status, 0)
-        self.assert_graphql(
-            gbp, gbp.query.gbpcli.untag_build, machine="lighthouse", tag="prod"
-        )
+        self.assertEqual(publisher.tags(BUILD), [])
 
     def test_untag_with_build_number_gives_error(self, fixtures: Fixtures):
-        cmdline = "gbp tag -r lighthouse 9400 prod"
-        args = parse_args(cmdline)
-        gbp = fixtures.gbp
+        publisher.tag(BUILD, "prod")
 
-        status = tag(args, gbp, fixtures.console)
+        status = fixtures.gbpcli("gbp tag -r lighthouse 9400 prod")
 
         self.assertEqual(status, 1)
         self.assertEqual(
