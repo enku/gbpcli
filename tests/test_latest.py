df529c77e2b79079f88289edfd8c352031fb3658
Albert Hopkins <marduk@letterboxes.org>
2025-08-23 15:42:59 -0500

    tests: latest: use testkit.gbpcli instead of lib.gbp

--- tests/test_latest.py
+++ tests/test_latest.py
@@ -1,42 +1,25 @@
 """Tests for the latest subcommand"""
 
-# pylint: disable=missing-function-docstring,protected-access
+# pylint: disable=missing-docstring
 import gbp_testkit.fixtures as testkit
-from gbp_testkit.helpers import parse_args, print_command
 from unittest_fixtures import Fixtures, given
-
-from gbpcli.subcommands.latest import handler as latest
 
 from . import lib
 
 
-@given(lib.gbp, testkit.console, lib.local_timezone)
+@given(testkit.gbpcli, lib.local_timezone, lib.pulled_build)
 class LatestTestCase(lib.TestCase):
     """latest() tests"""
 
     def test(self, fixtures: Fixtures):
-        cmdline = "gbp latest lighthouse"
-        args = parse_args(cmdline)
-        gbp = fixtures.gbp
-        console = fixtures.console
-        lib.make_response(gbp, "latest.json")
-
-        print_command(cmdline, console)
-        status = latest(args, gbp, console)
+        status = fixtures.gbpcli("gbp latest babette")
 
         self.assertEqual(status, 0)
-        expected = "$ gbp latest lighthouse\n3113\n"
-        self.assertEqual(console.out.file.getvalue(), expected)
-        self.assert_graphql(gbp, gbp.query.gbpcli.latest, machine="lighthouse")
+        expected = f"$ gbp latest babette\n{fixtures.build.build_id}\n"
+        self.assertEqual(fixtures.console.out.file.getvalue(), expected)
 
     def test_should_print_error_when_not_found(self, fixtures: Fixtures):
-        cmdline = "gbp latest bogus"
-        args = parse_args(cmdline)
-        gbp = fixtures.gbp
-        console = fixtures.console
-        lib.make_response(gbp, {"data": {"latest": None}})
-
-        status = latest(args, gbp, console)
+        status = fixtures.gbpcli("gbp latest bogus")
 
         self.assertEqual(status, 1)
         self.assertEqual(
