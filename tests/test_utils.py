a84e6c029ab13c959e01624913ec9205032f51af
Albert Hopkins <marduk@letterboxes.org>
2025-08-24 17:41:18 -0500

    tests: utils: use don't use lib.gbp fixture
    Instead either use testkit.gbp or a mock.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,8 @@ import sys
 from unittest import mock
 
 import gbp_testkit.fixtures as testkit
-from unittest_fixtures import Fixtures, given
+from gentoo_build_publisher import types as gbp_types
+from unittest_fixtures import Fixtures, given, where
 
 from gbpcli.graphql import APIError, check
 from gbpcli.types import Build
@@ -16,74 +17,50 @@ from gbpcli.utils import get_my_machines_from_args, load_env, re_path, resolve_b
 from . import lib
 
 
-@given(lib.gbp)
 class CheckTestCase(lib.TestCase):
     """check() tests"""
 
-    def test_should_raise_apierror_if_query_response_has_errors(
-        self, fixtures: Fixtures
-    ):
+    def test_should_raise_apierror_if_query_response_has_errors(self):
         error1 = {"message": "The end is near", "locations": [], "path": None}
         error2 = {"message": "Oh no!", "locations": [], "path": None}
-        response_with_errors = {"data": {"build": None}, "errors": [error1, error2]}
-        gbp = fixtures.gbp
-        gbp.query._session.post.return_value = lib.http_response(
-            json=response_with_errors
-        )
+        query_result = ({"build": None}, [error1, error2])
 
         with self.assertRaises(APIError) as context:
-            check(gbp.query.gbpcli.machines())
+            check(query_result)
 
         exception = context.exception
         self.assertEqual(exception.args[0], [error1, error2])
         self.assertEqual(exception.data, {"build": None})
 
 
-@given(lib.gbp)
+@given(testkit.gbp, lib.pulled_build)
+@where(pulled_build__build=gbp_types.Build(machine="lighthouse", build_id="123"))
 class ResolveBuildIdTestCase(lib.TestCase):
     """resolve_build_id() tests"""
 
     def test_returns_latest_build_for_machine_when_build_id_is_none(
         self, fixtures: Fixtures
     ):
-        gbp = fixtures.gbp
-        gbp.query._session.post.return_value = lib.http_response(
-            json={"data": {"latest": {"id": "lighthouse.123"}}}
-        )
-
-        result = resolve_build_id("lighthouse", None, gbp=gbp)
+        result = resolve_build_id("lighthouse", None, gbp=fixtures.gbp)
 
         self.assertEqual(result, Build(machine="lighthouse", number=123))
 
     def test_aborts_when_build_id_is_none_and_no_latest(self, fixtures: Fixtures):
-        gbp = fixtures.gbp
-        gbp.query._session.post.return_value = lib.http_response(
-            json={"data": {"latest": None}}
-        )
-
         with self.assertRaises(SystemExit) as context:
-            resolve_build_id("lighthouse", None, gbp=gbp)
+            resolve_build_id("bogus", None, gbp=fixtures.gbp)
 
-        self.assertEqual(context.exception.args, ("No builds for lighthouse",))
+        self.assertEqual(context.exception.args, ("No builds for bogus",))
 
     def test_returns_build_when_given_tag(self, fixtures: Fixtures):
-        gbp = fixtures.gbp
-        gbp.query._session.post.return_value = lib.http_response(
-            json={"data": {"resolveBuildTag": {"id": "lighthouse.123"}}}
-        )
+        fixtures.publisher.tag(fixtures.pulled_build, "prod")
 
-        result = resolve_build_id("lighthouse", "@prod", gbp=gbp)
+        result = resolve_build_id("lighthouse", "@prod", gbp=fixtures.gbp)
 
         self.assertEqual(result, Build(machine="lighthouse", number=123))
 
     def test_aborts_when_given_tag_that_does_not_exist(self, fixtures: Fixtures):
-        gbp = fixtures.gbp
-        gbp.query._session.post.return_value = lib.http_response(
-            json={"data": {"resolveBuildTag": None}}
-        )
-
         with self.assertRaises(SystemExit) as context:
-            resolve_build_id("lighthouse", "@prod", gbp=gbp)
+            resolve_build_id("lighthouse", "@prod", gbp=fixtures.gbp)
 
         self.assertEqual(context.exception.args, ("No such tag for lighthouse: prod",))
 
