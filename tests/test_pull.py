57bb39926ff550e3c9a0c9223dbddee37545cd84
Albert Hopkins <marduk@letterboxes.org>
2025-08-27 19:11:21 -0500

    tests: use a global variable
    Instead of defining the same immutable Build in each test.

--- tests/test_pull.py
+++ tests/test_pull.py
@@ -8,32 +8,31 @@ from gentoo_build_publisher import publisher
 from gentoo_build_publisher.types import Build
 from unittest_fixtures import Fixtures, given
 
+BUILD = Build(machine="lighthouse", build_id="3226")
+
 
 @given(testkit.gbpcli)
 class PullTestCase(TestCase):
     """pull() tests"""
 
     def test(self, fixtures: Fixtures):
-        build = Build(machine="lighthouse", build_id="3226")
         cmdline = "gbp pull lighthouse 3226"
 
         fixtures.gbpcli(cmdline)
 
-        self.assertTrue(publisher.pulled(build))
+        self.assertTrue(publisher.pulled(BUILD))
 
     def test_with_note(self, fixtures: Fixtures) -> None:
-        build = Build(machine="lighthouse", build_id="3226")
         cmdline = "gbp pull lighthouse 3226 --note='This is a test'"
 
         fixtures.gbpcli(cmdline)
 
-        record = publisher.record(build)
+        record = publisher.record(BUILD)
         self.assertEqual(record.note, "This is a test")
 
     def test_with_tags(self, fixtures: Fixtures) -> None:
-        build = Build(machine="lighthouse", build_id="3226")
         cmdline = "gbp pull lighthouse 3226 --tag test"
 
         fixtures.gbpcli(cmdline)
 
-        self.assertEqual(publisher.tags(build), ["test"])
+        self.assertEqual(publisher.tags(BUILD), ["test"])
